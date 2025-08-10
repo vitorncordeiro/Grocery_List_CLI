fb508388473087bb84430d7f40d8908b010bcfcd
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-08-10 12:20:58 -0300

    Update Lista.java

--- Lista.java
+++ Lista.java
@@ -4,33 +4,32 @@ public class Lista {
     private int length = 0;
     private ArrayList<String> lista = new ArrayList<>();
 
-    public int getLength(){
+    public int getLength() {
         return length;
     }
-    public void addItem(String item){
+
+    public void addItem(String item) {
         lista.add(item);
-        System.out.printf("Item %s adicionado com sucesso", item);
+        System.out.printf("Item %s added successfully", item);
         System.out.println();
         this.printList();
     }
-    public void removeItem(String item){
-        if(lista.contains(item)){
-            lista.remove(item);
-            System.out.println("Item removido com sucesso");
-            this.printList();
-        }else{
-            System.out.println("Item não encontrado");
-        }
 
+    public void removeItem(String item) {
+        if (lista.contains(item)) {
+            lista.remove(item);
+            System.out.println("Item removed successfully");
+            this.printList();
+        } else {
+            System.out.println("Item not found");
+        }
     }
-    public void printList(){
+
+    public void printList() {
         System.out.println("---------------------");
         for (String s : lista) {
             System.out.println(s);
         }
         System.out.println("---------------------");
     }
-
 }
-    
- 
